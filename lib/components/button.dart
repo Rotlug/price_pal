901d518019d2646fc0e9602afba6b73b14043d84
rotlug <rotlug77@gmail.com>
2024-10-23 16:15:20 +0300

    Add inner shadows to button and TextField

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide BoxShadow, BoxDecoration;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 
 enum DecoratedButtonStyle { normal, suggested }
 
@@ -15,33 +16,84 @@ class DecoratedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ButtonStyle(
-        elevation: const WidgetStatePropertyAll(0),
-        minimumSize: const WidgetStatePropertyAll(Size(double.infinity, 0)),
-        backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-            (Set<WidgetState> states) {
-          if (states.contains(WidgetState.disabled)) {
-            return Theme.of(context).colorScheme.onSecondaryFixed;
-          } else if (!suggestedAction) {
-            return Colors.transparent;
-          }
-          return Theme.of(context).colorScheme.primaryContainer;
-        }),
-        foregroundColor: WidgetStateProperty.resolveWith((states) {
-          if (suggestedAction) {
-            return Colors.white;
-          }
-          return null;
-        }),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: onPressed,
+          style: ButtonStyle(
+            minimumSize: const WidgetStatePropertyAll(Size(double.infinity, 0)),
+            padding: const WidgetStatePropertyAll(EdgeInsets.all(0)),
+            visualDensity: VisualDensity.compact,
+            backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+                (Set<WidgetState> states) {
+              if (states.contains(WidgetState.disabled)) {
+                return Theme.of(context).colorScheme.onSecondaryFixed;
+              } else if (!suggestedAction) {
+                return Colors.transparent;
+              }
+              return Theme.of(context).colorScheme.primaryContainer;
+            }),
+            foregroundColor: WidgetStateProperty.resolveWith((states) {
+              if (suggestedAction) {
+                return Colors.white;
+              }
+              return null;
+            }),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(text),
+          ),
+        ),
+        const ButtonInnerShadows(),
+      ],
+    );
+  }
+}
 
-        shape: WidgetStatePropertyAll(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Text(text),
+class ButtonInnerShadows extends StatelessWidget {
+  const ButtonInnerShadows({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: IgnorePointer(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow( // Black BoxShadow
+                    inset: true,
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 2,
+                    spreadRadius: 0,
+                    offset: const Offset(0, 2),
+                  ),
+                  BoxShadow( // White BoxShadow
+                    inset: true,
+                    color: Colors.white.withOpacity(0.15),
+                    blurRadius: 5,
+                    spreadRadius: 2,
+                    offset: const Offset(0, -2),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border:
+                    Border.all(color: Colors.white.withOpacity(0.05), width: 1),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/pages/setup_page.dart
+++ lib/pages/setup_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:price_pal/components/button.dart';
 import 'package:price_pal/components/camera_view.dart';
-import 'package:price_pal/components/container.dart';
 import 'package:price_pal/components/split_page.dart';
 import 'package:price_pal/pages/camera_page.dart';
 import 'package:price_pal/providers/storage_provider.dart';
@@ -87,23 +86,22 @@ class InputTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedContainer(
-      radius: 8,
-      child: Container(
-        color: const Color(0xff272727),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Insert your api key",
-                hintStyle: Theme.of(context).textTheme.displayMedium
-              ),
-              onChanged: onChanged,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Stack(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              filled: true,
+              fillColor: const Color(0xff272727),
+              hintText: "Insert your api key",
+              hintStyle: Theme.of(context).textTheme.displayMedium
             ),
+            onChanged: onChanged,
           ),
-        ),
+          const ButtonInnerShadows()
+        ],
       ),
     );
   }
